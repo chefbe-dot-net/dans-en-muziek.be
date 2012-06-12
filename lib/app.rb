6dd35ee2a34bd108f0aaed2bbbaf6c193d2ace4a
Bernard Lambeau <blambeau@gmail.com>
2012-06-12 18:30:29 +0200

    Simplify and move some tools to polygon.

--- lib/app.rb
+++ lib/app.rb
@@ -6,14 +6,6 @@ require 'dialect'
 class App < Polygon::Base
   helpers Helpers
 
-  ############################################################## Configuration
-  configure do
-    set :root,  Path.backfind('.[config.ru]')
-    set :wlang, :layout => :"layouts/html5"
-  end
-
-  ############################################################## Normal routes
-
   get '/sitemap.xml' do
     content_type "application/xml"
     wlang :sitemap, :locals => sitemap_locals, :layout => false
@@ -22,8 +14,6 @@ class App < Polygon::Base
   get "/" do
     wlang :index, :locals => index_locals
   end
-
-  ############################################################## Error handling
 
   # error handling
   error do
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,22 +1,16 @@
 module Helpers
 
-  # Converts a path to a file in dynamic content
-  def path2file(path)
-    path == "" ? dynamic/"index.yml" : nil
-  end
-
-  # Convert a file from dynamic content to a path
-  def file2path(file)
-    path = (file % dynamic).rm_ext
-    path = path.basename.to_s == "index" ? path.parent : path
+  def entry2path(entry)
+    path = entry.relative_path.rm_ext
+    path = entry.index? ? path.parent : path
     path == Path('.') ? Path("") : path
   end
 
   def sitemap
-    file2path = lambda{|f| file2path(f) }
+    entry2path = lambda{|f| entry2path(f) }
     lispy do
       (extend :entries,
-              :path    => proc{ file2path.call(entry.path)            },
+              :path    => proc{ entry2path.call(entry)                },
               :lastmod => proc{ entry.path.mtime.strftime("%Y-%m-%d") })
     end
   end
