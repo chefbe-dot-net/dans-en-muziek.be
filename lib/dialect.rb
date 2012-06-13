e9fb4a9d64b51e20213878f5dfd4cebc90bd7f32
Bernard Lambeau <blambeau@gmail.com>
2012-06-13 22:53:59 +0200

    Use new support for sinatra partials in wlang.

--- lib/dialect.rb
+++ lib/dialect.rb
@@ -5,21 +5,7 @@ class Dialect < WLang::Html
     buf << context.markdown(text)
   end
 
-  def greater(buf, fn)
-    super
-  rescue NameError
-    path = partial_path(render(fn))
-    buf << context.wlang(path.to_sym, :locals => scope.subject, :layout => false)
-  end
-
   tag '~', :tilde
-  tag '>', :greater
-
-  private
-
-    def partial_path(path)
-      (Path(template.path).parent / path) % Path(context.settings.views)
-    end
 
   Tilt.prefer Tilt::WLangTemplate.with_options(:dialect => Dialect), 'wlang'
 end
