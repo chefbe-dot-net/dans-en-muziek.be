4f9a1a232fb6880191549ee865c2625c7daec66f
Bernard Lambeau <blambeau@gmail.com>
2013-02-08 09:16:50 +0100

    Upgrade to edge polygon.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -6,27 +6,8 @@ module Helpers
     path == Path('.') ? Path("") : path
   end
 
-  def sitemap
-    entry2path = lambda{|f| entry2path(f) }
-    lispy do
-      (extend :entries,
-              :path    => proc{ entry2path.call(entry)                },
-              :lastmod => proc{ entry.path.mtime.strftime("%Y-%m-%d") })
-    end
-  end
-
   def sitemap_locals
-    { :urls => sitemap }
-  end
-
-  def index_locals
-    sitemap = self.sitemap
-    lispy do
-      (unwrap \
-        (extend (restrict sitemap, :path => Path("")),
-                :data => lambda{ entry.to_hash }),
-        :data)
-    end.to_a.first
+    { :urls => database.query{ sitemap } }
   end
 
 end
